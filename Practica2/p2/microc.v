42839c971bb934e89677dc87a392261fc8d5d2a5
Juan Siverio <alu0100585704@ull.edu.es>
2019-10-30 14:24:41 +0000

    agregados ficheros practica 2

---
+++ Practica2/p2/microc.v
@@ -0,0 +1,6 @@
+module microc(input wire clk, reset, s_abs, s_inc, s_inm, we3, wez, input wire [2:0] op, output wire z, output wire [5:0] opcode);
+//Microcontrolador sin memoria de datos de un solo ciclo
+
+//Instanciar e interconectar pc, memprog, regfile, alu, sum, biestable Z y mux's
+
+endmodule
