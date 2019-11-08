6f1c6fc38d8d311dadf306bba6a7c59e9cc06a47
Juan Siverio <alu0100585704@ull.edu.es>
2019-11-08 11:22:46 +0000

    corregido problemas con ficheros

--- Practica2/p2/CPU_entrada_salida/compilar_simular.cmd
+++
@@ -1,5 +0,0 @@
-rem iverilog -Wall -o cpu cpu_tb.v cpu.v cd.v uc.v alu.v componentes.v memprog.v in_out.v
-iverilog  -o cpu cpu_tb.v cpu.v cd.v uc.v alu.v componentes.v memprog.v in_out.v
-vvp cpu
-rem gtkwave cpu_tb.vcd
-pause
--- Practica2/p2/CPU_sin_ampliaciones/compilar_simular.cmd
+++
@@ -1,4 +0,0 @@
-iverilog  -o cpu cpu_tb.v cpu.v cd.v uc.v alu.v componentes.v memprog.v
-vvp cpu
-rem gtkwave cpu_tb.vcd
-pause
